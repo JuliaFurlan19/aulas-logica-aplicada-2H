c24aa2ff9db61f0c42197089f68b9cb292e369e1
JuliaFurlan19 <julialeitefurlan@gmail.com>
2023-04-13 08:17:39 -0300

    Criado laço while e for.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int contador = 0;
+
+        while(contador < 10){
+            System.out.println("Contador = " + contador);
+
+            // Incremento
+            contador++;
+        }
+
+        // Laço para faça
+        for(int x = 0; x < 10; x++){
+            System.out.println("Contador = " + x);
+        }
     }
 }
